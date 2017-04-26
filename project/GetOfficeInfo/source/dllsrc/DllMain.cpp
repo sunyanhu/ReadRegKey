c6647bc999a51f15418336f5dbabdc20821ac55e
sunyanhu <sunyanhu@sbr-info.com>
2017-04-26 14:34:32 +0800

    制表符修改

--- project/GetOfficeInfo/source/dllsrc/DllMain.cpp
+++ project/GetOfficeInfo/source/dllsrc/DllMain.cpp
@@ -5,15 +5,15 @@ BOOL WINAPI DllMain(HINSTANCE hinstDLL,
 	DWORD  dwReason,
 	LPVOID lpvReserved)
 {
-	switch (dwReason)
-	{
-	case DLL_PROCESS_ATTACH:
-		g_VersionInstance = hinstDLL;
-		break;
-	case DLL_THREAD_ATTACH:
-	case DLL_THREAD_DETACH:
-	case DLL_PROCESS_DETACH:
-		break;
-	}
-	return TRUE;
+    switch (dwReason)
+    {
+    case DLL_PROCESS_ATTACH:
+        g_VersionInstance = hinstDLL;
+        break;
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
 }
